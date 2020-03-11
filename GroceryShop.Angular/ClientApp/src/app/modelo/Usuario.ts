78e9af1be8ed2e193307eca3a596c82d31a933df
Emerson Bilieri Claudelino <ebilieri@gmail.com>
2020-03-10 22:23:21 -0300

    adicionar campo documento form usuario

--- GroceryShop.Angular/ClientApp/src/app/modelo/Usuario.ts
+++ GroceryShop.Angular/ClientApp/src/app/modelo/Usuario.ts
@@ -1,8 +1,10 @@
 export class Usuario {
   id: number;
+  tipoPessoa: number;
   email: string;
   senha: string;
   nome: string;
   sobreNome: string;
+  documento: string;
   ehAdministrador: boolean;
 }
